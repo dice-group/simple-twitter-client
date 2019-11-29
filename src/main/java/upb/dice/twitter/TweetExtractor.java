6c20476a8536ed4983acfe084a02593dfa695c39
anirudh <aka.anirudh@gmail.com>
2019-11-29 15:28:02 +0100

    Extracts all the Tweets even if the rate limit has reached

--- src/main/java/upb/dice/twitter/TweetExtractor.java
+++ src/main/java/upb/dice/twitter/TweetExtractor.java
@@ -2,55 +2,81 @@ package upb.dice.twitter;
 
 import twitter4j.*;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * If the rate limit is not reached, extract tweets based on the keywords or location (provided as latitude, longitude and the radius of coverage)
  * and stores them in a file. Also maintains a trackrecord in a file of the maxID obtained in every search
  */
 public class TweetExtractor {
+
     private StoreMaxID storeMaxID = new StoreMaxID();
     private Twitter twitter = TwitterFactory.getSingleton();
-    private RateLimitChecker rateLimitChecker = new RateLimitChecker();
-    private boolean check;
+    private boolean remainingTweet;
+
+    public boolean rateLimit;
 
     /**
      * Recursive search for tweets based on this query and store the maxID for the search results
      *
      * @param query search based on this query
      */
-    public void getTweet(Query query) throws IOException, TwitterException {
+    public void getTweet(Query query) throws InterruptedException {
         QueryResult queryResult;
-        check = rateLimitChecker.rateLimitCheck();
-        System.out.println("Tweets search Start");
-        FileWriter writer = new FileWriter("Tweets.txt", true);
-        int counter = 0;
         try {
+            rateLimit = new RateLimitChecker().rateLimitCheck();
+            System.out.println("Tweets search Start");
+            String directoryName = "Tweets Search Details";
+            FileWriter writer = new FileWriter(new File(directoryName + File.separator + "Tweet object.txt"), true);
+            int counter = 0;
+            long sinceID = 0;
+            long oldestTweetID;
             do {
-                check = rateLimitChecker.rateLimitCheck();
                 queryResult = twitter.search(query);
                 List<Status> queryTweets = queryResult.getTweets();
-                long maxId;
-                if (counter == 0) {
-                    maxId = queryResult.getMaxId();
-                    if (query.getQuery() == null) {
-                        storeMaxID.locationBasedMaxID(maxId, query);
-                    } else {
+                long maxId = queryResult.getMaxId();
+
+                //the first page and the first time
+                if (counter == 0 && !remainingTweet) {
+                    sinceID = queryResult.getSinceId();
+                    if (query.getQuery() != null) {
                         storeMaxID.keywordBasedMaxID(maxId, query);
                     }
+                    else {
+                        storeMaxID.locationBasedMaxID(maxId, query);
+                    }
+                }
+                oldestTweetID = 0;
+                for (Status i : queryTweets) {
+                    oldestTweetID = i.getId();
+                    writer.append(i.getText());
                 }
                 counter++;
-                for (Status i : queryTweets) {
-                    writer.append(i.toString());
-                }
+                rateLimit = new RateLimitChecker().rateLimitCheck();
             }
-            while (((query = queryResult.nextQuery()) != null) && (check));
-        } catch (TwitterException | IOException e) {
-            System.out.println("Please try with proper authentication data");
+            while (((query = queryResult.nextQuery()) != null) && (!rateLimit) && oldestTweetID > sinceID);
+
+            //if the rate limit has reached and there is a gap between the base and the current tweet, redo the search with the oldest tweet as the upper bound
+            if (oldestTweetID - sinceID > 0 && rateLimit && query != null) {
+                query.setSinceId(sinceID);
+                query.setUntil(String.valueOf(oldestTweetID));
+                remainingTweet = true;
+                rateLimit = false;
+                System.out.println("In extracting the remaining tweets. Current time is: " + new Date());
+                TimeUnit.MINUTES.sleep(15);
+                getTweet(query);
+            }
+            writer.close();
+        } catch (TwitterException | IOException | InterruptedException e) {
+            System.out.println(e);
+            rateLimit = true;
+            TimeUnit.MINUTES.sleep(15);
         }
         System.out.println("Tweets search End");
-        writer.close();
     }
 }
