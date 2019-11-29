0076bbba6ce9959b3d7cf843d4a32e455ce81c14
anirudh <aka.anirudh@gmail.com>
2019-11-29 15:30:12 +0100

    Capable of handling the arguments passed together before the start of search. Toggles between the keyword and location based searches.

--- src/main/java/upb/dice/twitter/TweetSearchApplication.java
+++ src/main/java/upb/dice/twitter/TweetSearchApplication.java
@@ -1,88 +1,155 @@
 package upb.dice.twitter;
 
 import org.apache.commons.cli.*;
-import twitter4j.TwitterException;
 
 import java.io.IOException;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.*;
 
 /**
  * An application which extracts tweets based on a specific keyword or location provided by the user.
- * 1) If keyword based: The keyword has to be mentioned along with the time of periodic execution (in hours) is provided
- * 2) If location based: The latitude, longitude and the radius along with the time of periodic execution (in hours) is provided
+ * The input consists of the keywords and location list passed as command line arguments together along with the period of execution
+ * Initially all the available tweets from the past 7 days are extracted. In the consecutive batches only the newest tweets are extracted
  */
 public class TweetSearchApplication {
 
-    private static String key = null;
-    private static double latitude, longitude, radius = -1;
-
     public static void main(String[] args) {
+
+        // original queue of keywords
+        Queue<String> keywordQueue = new LinkedList<>();
+
+        //original queue of location details (radius, latitude and longitude)
+        Queue<Double> radiusQeue = new LinkedList<>();
+        Queue<Double> latitudeQeue = new LinkedList<>();
+        Queue<Double> longitudeQeue = new LinkedList<>();
+
+        //the period of execution aka the time of the every batch
         final long[] checkPeriod = {60 * 60 * 1000};
 
         //initialize the parser
-        CommandLineParser parser = new DefaultParser();
+        CommandLineParser defaultParser = new DefaultParser();
 
         //create available options
-        Option option1 = Option.builder("k").hasArg(true).numberOfArgs(2).desc("Keyword Based Search").longOpt("key").argName("Keyword><Check period (In hours)").build();
-        Option option2 = Option.builder("l").longOpt("loc").hasArg(true).numberOfArgs(4).desc("Location Based Search").argName("Latitude><Longitude><Radius In Kilometers><Check period (In hours)").valueSeparator(' ').build();
+        Option option1 = Option.builder("k").hasArg(true).desc("Keyword Based Search").numberOfArgs(Option.UNLIMITED_VALUES).longOpt("key").valueSeparator(' ').argName("Keyword").build();
+        Option option2 = Option.builder("l").longOpt("loc").hasArg(true).desc("Location Based Search").numberOfArgs(Option.UNLIMITED_VALUES).argName("Latitude><Longitude><Radius In Kilometers><Check period (In hours)").valueSeparator(' ').build();
         Option option3 = Option.builder("h").longOpt("Help").hasArg(false).desc("Help Menu").build();
 
         //prepare the options
         Options options = new Options();
         options.addOption(option1).addOption(option2).addOption(option3);
-
         CommandLine cmd;
         try {
-            cmd = parser.parse(options, args); //read in the options
+
+            cmd = defaultParser.parse(options, args); //read in the options
+
             //helper option
             if (cmd != null && cmd.hasOption("h")) {
                 HelpFormatter formatter = new HelpFormatter();
                 formatter.printHelp("Command Line Helper", options);
             }
             //either keyword or location based
-            else if ((cmd != null && cmd.hasOption("k")) || (cmd != null && cmd.hasOption("l"))) {
-                if (cmd.hasOption("k")) {
-                    String[] searchArgs = cmd.getOptionValues("k");
-                    key = searchArgs[0];
-                    long periodSeconds = Long.parseLong(searchArgs[1]);
-                    checkPeriod[0] = periodSeconds * 60 * 60 * 1000;
-                } else if (cmd.hasOption("l")) {
-                    String[] searchArgs = cmd.getOptionValues("l");
-                    latitude = Double.parseDouble(searchArgs[0]);
-                    longitude = Double.parseDouble(searchArgs[1]);
-                    radius = Double.parseDouble(searchArgs[2]);
-                    long periodSeconds = Long.parseLong(searchArgs[3]);
-                    checkPeriod[0] = periodSeconds * 60 * 60 * 1000;
-                }
-                Timer timer = new Timer();
-                timer.schedule(new TimerTask() {
-                    TweetExplorer searchTweets = new TweetExplorer();
 
-                    @Override
-                    public void run() {
-                        if (key != null) {
-                            try {
-                                searchTweets.keywordQuery(key);
-                            } catch (IOException | InterruptedException | TwitterException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                        if (latitude != -1 && longitude != -1 && radius != -1) {
-                            try {
-                                searchTweets.locationQuery(latitude, longitude, radius);
-                            } catch (IOException | TwitterException | InterruptedException e) {
-                                e.printStackTrace();
-                            }
+            else {
+                if (cmd != null && (cmd.hasOption("k") || cmd.hasOption("l"))) {
+                    if (cmd.hasOption("k")) {
+                        String[] searchArgs = cmd.getOptionValues("k");
+                        keywordQueue.addAll(Arrays.asList(searchArgs));
+
+                    }
+                    if (cmd.hasOption("l")) {
+                        String[] searchArgs = cmd.getOptionValues("l");
+                        int j = 0;
+                        while (j < (searchArgs.length - 1)) {
+                            latitudeQeue.add(Double.parseDouble(searchArgs[j]));
+                            longitudeQeue.add(Double.parseDouble(searchArgs[j + 1]));
+                            radiusQeue.add(Double.parseDouble(searchArgs[j + 2]));
+                            long periodSeconds = Long.parseLong(searchArgs[searchArgs.length - 1]);
+                            checkPeriod[0] = periodSeconds * 60 * 60 * 1000;
+                            j = j + 3;
+
                         }
                     }
 
-                }, 0, checkPeriod[0]);
-                System.out.println("Running at: " + new java.util.Date());
-            }
-        } catch (ParseException e) {
-            System.out.println("Error in arguments. " + e.getMessage());
-        }
+                    Timer timer1 = new Timer();
+                    timer1.schedule(new TimerTask() {
 
+                        TweetExplorer searchTweets = new TweetExplorer();
+
+                        @Override
+                        public void run() {
+                            Iterator iterator;
+                            Iterator iterator1, iterator2, iterator3;
+
+                            //initialize a new queue by copying from the original queue
+                            LinkedList<String> currentLKey = new LinkedList<>(keywordQueue);
+                            LinkedList<Double> currentLat = new LinkedList<>(latitudeQeue);
+                            LinkedList<Double> currentLon = new LinkedList<>(longitudeQeue);
+                            LinkedList<Double> currentLRad = new LinkedList<>(radiusQeue);
+
+
+                            //get iterators for the queue
+                            iterator = currentLKey.iterator();
+                            iterator1 = currentLat.iterator();
+                            iterator2 = currentLon.iterator();
+                            iterator3 = currentLRad.iterator();
+
+                            //runs forever with the queues being assigned inside the while loop
+                            while ((iterator.hasNext()) || (iterator1.hasNext() && iterator2.hasNext() && iterator3.hasNext())) {
+
+
+                                //start keyword based search
+                                while (iterator.hasNext()) {
+                                    String keyCurrent = currentLKey.peek();
+                                    System.out.println("Current keyword is: " + keyCurrent);
+
+                                    try {
+
+                                        searchTweets.keywordQuery(keyCurrent); //Start the search
+                                        currentLKey.remove(); //Remove from the current queue
+                                    } catch (IOException | InterruptedException ex) {
+                                        ex.printStackTrace();
+                                    }
+                                }
+
+
+                                //start location based search
+                                while (iterator1.hasNext() && iterator2.hasNext() && iterator3.hasNext()) {
+                                    Double lat = currentLat.peek();
+                                    Double lon = currentLon.peek();
+                                    Double rad = currentLRad.peek();
+                                    System.out.println("Current Latitude, Longitude and Radius: " + lat.toString() + "," + lon.toString() + ',' + rad.toString());
+                                    try {
+                                        searchTweets.locationQuery(lat, lon, rad); //Search start
+                                        //remove from the current queue
+                                        currentLat.remove();
+                                        currentLon.remove();
+                                        currentLRad.remove();
+
+                                    } catch (IOException | InterruptedException ex) {
+                                        ex.printStackTrace();
+                                    }
+                                }
+
+                                //initialize new queue by copying values from the original queue
+                                currentLKey = new LinkedList<>(keywordQueue);
+                                currentLat = new LinkedList<>(latitudeQeue);
+                                currentLon = new LinkedList<>(longitudeQeue);
+                                currentLRad = new LinkedList<>(radiusQeue);
+
+                                //get the iterator for the newly assigned queue
+                                iterator = currentLKey.iterator();
+                                iterator1 = currentLat.iterator();
+                                iterator2 = currentLon.iterator();
+                                iterator3 = currentLRad.iterator();
+
+                            }
+
+                        }
+                    }, 0, checkPeriod[0]);
+                    System.out.println("Running at: " + new Date());
+                }
+            }
+        } catch (ParseException | NumberFormatException e) {
+            e.printStackTrace();
+        }
     }
 }
