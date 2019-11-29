2a6620b33a681dfb9e9c52acc1cc7c5beea02700
anirudh <aka.anirudh@gmail.com>
2019-11-29 15:24:53 +0100

    Improved file handling

--- src/main/java/upb/dice/twitter/StoreMaxID.java
+++ src/main/java/upb/dice/twitter/StoreMaxID.java
@@ -3,6 +3,7 @@ package upb.dice.twitter;
 import twitter4j.Query;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -11,12 +12,14 @@ import java.io.IOException;
  */
 public class StoreMaxID {
 
+    String directoryName = "Tweets Search Details";
+
     /**
      * @param maxID the maxID retrieved from the current search
      * @param query the query containing the latitude and longitude
      */
     public void locationBasedMaxID(double maxID, Query query) throws IOException {
-        try (BufferedWriter out = new BufferedWriter(new FileWriter("Latitude_Longitude_MaxID1.txt", true))) {
+        try (BufferedWriter out = new BufferedWriter(new FileWriter(new File(directoryName + File.separator + "Latitude_Longitude_MaxID.txt"), true))) {
             out.append(String.valueOf(query.getGeocode())).append(",").append(String.valueOf(maxID));
             out.append('\n');
         }
@@ -27,7 +30,7 @@ public class StoreMaxID {
      * @param query the query containing the keyword
      */
     public void keywordBasedMaxID(double maxID, Query query) throws IOException {
-        try (BufferedWriter out = new BufferedWriter(new FileWriter("Keyword_MaxID.txt", true))) {
+        try (BufferedWriter out = new BufferedWriter(new FileWriter(new File(directoryName + File.separator + "Keyword_MaxID.txt"), true))) {
             out.append(query.getQuery()).append(",").append(String.valueOf(maxID));
             out.append('\n');
         }
