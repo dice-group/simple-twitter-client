46ca4d5ba8ba18d708aca2908ada290c67415164
anirudh <aka.anirudh@gmail.com>
2019-11-29 15:26:26 +0100

    Improved details of the lower bound (sinceID) for consecutive searches

--- src/main/java/upb/dice/twitter/TweetExplorer.java
+++ src/main/java/upb/dice/twitter/TweetExplorer.java
@@ -3,7 +3,6 @@ package upb.dice.twitter;
 import twitter4j.GeoLocation;
 import twitter4j.GeoQuery;
 import twitter4j.Query;
-import twitter4j.TwitterException;
 
 import java.io.IOException;
 
@@ -14,17 +13,16 @@ public class TweetExplorer {
 
     private MaxIDChecker maxIDChecker = new MaxIDChecker();
     private TweetExtractor tweetExtractor = new TweetExtractor();
-
-    /**
+        /**
      * This method builds a query based on the keyword and extracts the tweets containing this keyword
      *
      * @param key for which the query is built and the tweets containing this key are searched and extracted
      */
-    public void keywordQuery(String key) throws IOException, TwitterException, InterruptedException {
+    public void keywordQuery(String key) throws IOException, InterruptedException {
         long key_id_1 = maxIDChecker.keyParse(key);
         Query query1 = new Query(key);
         if (key_id_1 != 1) {
-            query1.sinceId(key_id_1);
+            query1.setSinceId(key_id_1);
         }
         tweetExtractor.getTweet(query1);
     }
@@ -36,13 +34,13 @@ public class TweetExplorer {
      * @param longitude of the location
      * @param radius    coverage from the location point
      */
-    public void locationQuery(double latitude, double longitude, double radius) throws IOException, TwitterException, InterruptedException {
+    public void locationQuery(double latitude, double longitude, double radius) throws IOException, InterruptedException {
         GeoQuery geoQuery = new GeoQuery(new GeoLocation(latitude, longitude));
         Query query = new Query();
         query.setGeoCode(geoQuery.getLocation(), radius, Query.KILOMETERS);
         long key_id_1 = maxIDChecker.locationParse(new GeoLocation(latitude, longitude));
         if (key_id_1 != 1) {
-            query.sinceId(key_id_1);
+            query.setSinceId(key_id_1);
         }
         tweetExtractor.getTweet(query);
     }
