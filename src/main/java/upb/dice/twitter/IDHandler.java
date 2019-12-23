99e2fd3d7fccca84ea4d64c8db4adb2ca89597ff
anirudhash <aka.anirudh@gmail.com>
2019-12-23 16:49:39 +0100

    Correct dependency

--- src/main/java/upb/dice/twitter/IDHandler.java
+++ src/main/java/upb/dice/twitter/IDHandler.java
@@ -11,9 +11,9 @@ import java.util.*;
  * during the next search
  */
 public class IDHandler {
-    private static String directoryName = "Tweets_Search_Details";
-    private static String keywordMaxIDFilepath = directoryName + File.separator + "Keyword_MaxID1.txt";
-    private static String locationMaxIDFilepath = directoryName + File.separator + "Location_MaxID1.txt";
+    private final static String directoryName = "Tweets_Search_Details";
+    private final static String keywordMaxIDFilepath = directoryName + File.separator + "Keyword_MaxID2.txt";
+    private final static String locationMaxIDFilepath = directoryName + File.separator + "Location_MaxID2.txt";
 
     /**
      * This method stores the maxID depending on the query
@@ -25,10 +25,12 @@ public class IDHandler {
         //keywordBased MaxID
         if (query.getQuery() != null) {
             try (BufferedWriter out = new BufferedWriter(new FileWriter(new File(keywordMaxIDFilepath), true))) {
+                out.newLine();
                 out.append(query.getQuery()).append(",").append(String.format("%.0f", maxID));
             }
         } else {
             try (BufferedWriter out = new BufferedWriter(new FileWriter(new File(locationMaxIDFilepath), true))) {
+                out.newLine();
                 out.append(String.valueOf(query.getGeocode())).append(",").append(String.format("%.0f", maxID));
             }
         }
@@ -42,60 +44,64 @@ public class IDHandler {
      */
     public List<Long> keyParse(String key) throws IOException {
         List<Long> list = new ArrayList<>();
-        list.add(-1L);
-        list.add(-1L);
-        list.add(-1L);
+        list.add((long) Double.POSITIVE_INFINITY);//maxID
+        list.add((long) Double.POSITIVE_INFINITY);//oldestTweetID
+        list.add(-1L);//sinceID
         File file = new File(keywordMaxIDFilepath);
         file.getParentFile().mkdirs();
         file.createNewFile();
         Scanner scan = new Scanner(file);
         while (scan.hasNext()) {
             String line = scan.nextLine();
-            if (line.contains(key)) {
+            if (line.contains(key) && line.length() < 4) {
                 String[] splitString = line.split(",");
-                list.set(0, Long.parseLong(splitString[1]));
-                list.set(1, Long.parseLong(splitString[2]));
-                list.set(2, Long.parseLong(splitString[3]));
+                //if the current oldestTweetID value is not null, replace with the new maxID and oldestTweetID value. Else return the previous value for both
+                if (splitString[2] != null) {
+                    list.set(0, Long.parseLong(splitString[1]));
+                    list.set(1, Long.parseLong(splitString[2]));
+                }
+                if (splitString[3] != null) //if sinceID is not available, set the previously available sinceID
+                    list.set(2, Long.parseLong(splitString[3]));
             }
         }
         return list;
     }
 
     /**
-     * this method searches the parameters stored which serves as the basis of query generation
+     * This method searches the parameters stored which serves as the basis of query generation
      *
      * @param geoLocation for which the parameters have to be checked
      * @return the maxID for the respective geoLocation
      */
     public List<Long> locationParse(GeoLocation geoLocation) throws IOException {
-        Map<List<String>, List<Long>> pairStringMap = new HashMap<>();
+        List<Long> list = new ArrayList<>();
+        list.add((long) Double.POSITIVE_INFINITY);//maxID
+        list.add((long) Double.POSITIVE_INFINITY);//oldestTweetID
+        list.add(-1L);//sinceID
         File file = new File(locationMaxIDFilepath);
         file.getParentFile().mkdir();
         file.createNewFile();
-        String line;
-        BufferedReader br = new BufferedReader(new FileReader(file));
-        while ((line = br.readLine()) != null) {
-            String[] splitString = line.split(",");
-            pairStringMap.put(Arrays.asList(splitString[0], splitString[1]), Arrays.asList(Long.parseLong(splitString[3]), Long.parseLong(splitString[4]), Long.parseLong(splitString[5])));
+        Scanner scan = new Scanner(file);
+        while (scan.hasNext()) {
+            String line = scan.nextLine();
+            if (line.contains((geoLocation.getLatitude() + "," + geoLocation.getLongitude())) && line.length() == 6) {
+                String[] splitString = line.split(",");
+                //if the current oldestTweetID value is not null, replace with the new maxID and oldestTweetID value. Else return the previous value for both
+                if (splitString[4] != null) {
+                    list.set(0, Long.parseLong(splitString[1]));
+                    list.set(1, Long.parseLong(splitString[2]));
+                }
+                if (splitString[5] != null) //if sinceID is not available, set the previously available sinceID
+                    list.set(2, Long.parseLong(splitString[3]));
+            }
         }
-        String lat_str = String.valueOf(geoLocation.getLatitude());
-        String lon_str = String.valueOf(geoLocation.getLongitude());
-        List<Long> IDList = new ArrayList<>();
-        if (pairStringMap.containsKey(Arrays.asList(lat_str, lon_str))) {
-            IDList = pairStringMap.get(Arrays.asList(lat_str, lon_str));
-        } else {
-            IDList.add(-1L);
-            IDList.add(-1L);
-            IDList.add(-1L);
-
-        }
-        return IDList;
+        return list;
     }
 
     /**
      * Stores the oldestTweetID for a given query
      *
-     * @param oldestTweetID
+     * @param oldestTweetID the ID of the last tweet which was extracted at this instance of search
      * @param query         to which the parameters have to be stored
      */
     public void writeOldestTweetID(double oldestTweetID, Query query) throws IOException {
@@ -113,18 +119,17 @@ public class IDHandler {
     /**
      * Stores the sinceID for a given query
      *
-     * @param sinceID
+     * @param sinceID the lower bound of the tweet after which the tweets are to be extracted
      * @param query   to which the parameters have to be stored
-     * @throws IOException
      */
     public void writeSinceID(double sinceID, Query query) throws IOException {
         if (query.getQuery() != null) {
             try (BufferedWriter out = new BufferedWriter(new FileWriter(new File(keywordMaxIDFilepath), true))) {
-                out.append(",").append(String.format("%.0f", sinceID)).append('\n');
+                out.append(",").append(String.format("%.0f", sinceID));
             }
         } else {
             try (BufferedWriter out = new BufferedWriter(new FileWriter(new File(locationMaxIDFilepath), true))) {
-                out.append(",").append(String.format("%.0f", sinceID)).append('\n');
+                out.append(",").append(String.format("%.0f", sinceID));
             }
         }
     }
