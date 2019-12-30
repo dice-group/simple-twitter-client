cf849c05c61cdb85b0243688fe66dfb4bff13c50
anirudhash <aka.anirudh@gmail.com>
2019-12-30 19:45:45 +0100

    An interface for extracting data

---
+++ src/main/java/upb/dice/twitter/DataExtractor.java
@@ -0,0 +1,12 @@
+package upb.dice.twitter;
+
+import twitter4j.Query;
+
+public interface DataExtractor {
+    /**
+     * A method which writes data objects to a file
+     * @param query
+     */
+    void getData(Query query);
+
+}
