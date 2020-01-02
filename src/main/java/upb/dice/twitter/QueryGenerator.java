3e7272749a96607e323689cd92f392107545f9f9
anirudhash <aka.anirudh@gmail.com>
2020-01-02 18:06:20 +0100

    Usage of setUntil has no effect during the query generation. Hence using the setMaxID instead

--- src/main/java/upb/dice/twitter/QueryGenerator.java
+++ src/main/java/upb/dice/twitter/QueryGenerator.java
@@ -14,9 +14,8 @@ public abstract class QueryGenerator {
     public Query getModifiedQuery(Query query) throws IOException {
         List<Long> detailesID = idHandler.retrieveCurrentState(query);
         long maxID = detailesID.get(0), oldestTweetID = detailesID.get(1), sinceID = detailesID.get(2);
-        if (oldestTweetID - sinceID > 0) {
-            query.setSinceId(sinceID);
-            query.setUntil(String.valueOf(oldestTweetID));
+        if (Math.abs(oldestTweetID) - Math.abs(sinceID) > 0) {
+            query.setMaxId(oldestTweetID);
         } else {
             query.setSinceId(maxID);
         }
